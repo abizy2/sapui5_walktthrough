d197a5354ee29e58db15bd5b9fc0797d82c8b73b
abizy2 <34310497+abizy2@users.noreply.github.com>
2020-04-22 14:35:41 +0300

    ed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+app.use(express.static(__dirname));
 app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
