9d42fe2195decc9c98630b8201b49021021cb5dc
abizy2 <34310497+abizy2@users.noreply.github.com>
2020-08-04 17:58:43 +0300

    e

--- controller/busket.controller.js
+++ controller/busket.controller.js
@@ -11,23 +11,33 @@ sap.ui.define([
   //"use strict";
 
   return Controller.extend('namespace.controller.busket', {
-
-	  
-
     onAdd: function(oEvent) {
-	var sBusketRowPath = oEvent.getBindingContext("main").getPath();
+	var sBusketRowPath = oEvent.getSource().getBindingContext("main").getPath();// путь массива
 	let oModel = oEvent.getSource().getModel('main')
-	var oBusketIttem = oModel.getProperty(sBusketRowPath)
-	
-	
-	//getQuantity++;
-	//oModel.setProperty("/busket", getQuantity)
+	var oBusketItem = oModel.getProperty(sBusketRowPath)// строка
+	var Quantity = oBusketItem.Quantity
+	//console.dir(sBusketRowPath);
+	Quantity++;
+	oModel.setProperty(sBusketRowPath+"/Quantity", Quantity)
     },
 	
 	
 	
 	onDelete: function(oEvent) {
-		
-    }
+		var sBusketRowPath = oEvent.getSource().getBindingContext("main").getPath();
+		let oModel = oEvent.getSource().getModel('main')
+		var oBusketItem = oModel.getProperty(sBusketRowPath)
+		var Quantity = oBusketItem.Quantity
+		var aBusketData = oModel.getProperty("/busket"); // массив все данных элемента
+		//console.dir(sBusketRowPath);
+		Quantity--;
+		if (oBusketItem.Quantity > 0) {
+			oModel.setProperty(sBusketRowPath + "/Quantity", Quantity)
+		}
+		aBusketData.filter(function(oBusketItem)) {
+				return oBusketItem.Quantity < 1;
+			}
+			
+		}
   });
 });
